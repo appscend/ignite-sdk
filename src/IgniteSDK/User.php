79d119db058b9dd6f164448e64ab51d70ecc45b3
Răzvan Botea <razvan@appscend.com>
2014-12-17 16:51:12 +0200

    fix bug

--- src/IgniteSDK/User.php
+++ src/IgniteSDK/User.php
@@ -53,8 +53,10 @@ class User extends ResponseObject {
 		$result = json_decode($result, true);
 		if (curl_getinfo(self::$curl, CURLINFO_HTTP_CODE) >= 400)
 			return null;
-		if ($result['status'] >= 400)
+		if ($result['status'] >= 400) {
 			throw new \Exception($result['content']);
+			return false;
+		}
 
 		return $result['content'];
 	}
